fd92851efce39ce1d852083b34b34d1bcb12027b
snowmanly101 <dbtttttttfoot@gmail.com>
2026-08-14 04:11:26 -0700

    Enable debug mode

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3yrli1jdw5qq@8k9mji4gnttz3@ian3=r-%=v@u)6q_12*18@18n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'treasury-portal.onrender.com', 
