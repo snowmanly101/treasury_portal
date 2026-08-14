0d5215427146f969c7f4380e7a5c2274bdc9314c
snowmanly101 <dbtttttttfoot@gmail.com>
2026-08-13 18:52:22 -0700

    Update settings for production deployment

--- core/settings.py
+++ core/settings.py
@@ -13,19 +13,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(_file_).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3yr!i$dw5qg@8k9mj4gnttz3)&i&n3=r=%=v@u)6q_!2*!8@in'
+SECRET_KEY = 'django-insecure-3yrli1jdw5qq@8k9mji4gnttz3@ian3=r-%=v@u)6q_12*18@18n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['treasury-portal.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
